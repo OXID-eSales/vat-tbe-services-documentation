dd447968bd02eadca43ed1ab7efcdec0252b05b8
Tatyana Koleva <tatjana.koleva@dnhsoft.com>
2024-07-01 15:38:31 +0300

    Add new 4.0 version link to switch to

--- conf.py
+++ conf.py
@@ -137,6 +137,7 @@ html_context = {
     'versions':
         [('2.1', 'https://docs.oxid-esales.com/modules/vat-tbe-services/de/2.1/'),
         ('3.0', 'https://docs.oxid-esales.com/modules/vat-tbe-services/de/3.0/'),
+        ('4.0', 'https://docs.oxid-esales.com/modules/vat-tbe-services/de/4.0/'),
          ],
     'languages':
        [('DE', 'https://docs.oxid-esales.com/modules/vat-tbe-services/de/2.1/'),
